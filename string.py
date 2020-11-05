eb4394ba071e3cf50a43e0e7debbb4b683a4db47
KoksalBerkay <koksalberkaydenktas@gmail.com>
2020-11-05 19:31:07 +0300

    lists.py added

--- string.py
+++ string.py
@@ -1,9 +1,37 @@
-name = "Köksal Berkay DENKTAŞ"
-################## STRİNG METHODS #####################
+name = "ilhan koral"
 
-#Title method
+print("name:" , name)
 
-print(name.title())
+print(name.capitalize())
+
+name_title = name.title()
+
+name_cap = name.capitalize()
+
+#print("new_name:" , new_name)
+
+print("name_title:" , name_title)
+print("name_cap:" , name_cap) # ctrl + space open intellisense
 
 
-print(name)
+# # concentation
+
+first_name = "Koksal"
+last_name = "Denktas"
+the_best_friend = "Cihangir"
+
+full_name = first_name + " " + last_name
+
+story = full_name + " has " + the_best_friend + "as a bets friend."
+
+print(story)
+
+print('Cihangir sabah herkese "Gunaydin" dedi. ') #Escape character"
+
+print(full_name + " \nhas " + the_best_friend + "\n\tas a best friend")
+
+multiline_text = '''Merhaba
+benim adım ilhan
+Nasilsiniz?'''
+
+print(multiline_text)
