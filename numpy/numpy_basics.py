5074783c1763a7c0af2d0a41093e30591f0ac949
KoksalBerkay <koksalberkaydenktas@gmail.com>
2022-04-14 13:33:17 +0300

    learning numpy basics

--- numpy/numpy_basics.py
+++ numpy/numpy_basics.py
@@ -2,7 +2,7 @@
 import numpy as np
 from tables import DataTypeWarning
 
-### numpy basics
+##### Numpy Basics #####
 array = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]) # 1*15 vector
 
 print(array.shape)
@@ -71,3 +71,68 @@ print(np.sqrt(a)) # square root
 print(np.square(a)) # a**2
 
 print(np.add(a,a))
+
+##### Indexing and Slicing #####
+
+array = np.array([1,2,3,4,5,6,7]) # vector dimension = 1
+print(array[0])
+
+print(array[0:4])
+
+reverse_array = array[::-1]
+print(array)
+print(reverse_array)
+
+array1 = np.array([[1,2,3,4,5],[6,7,8,9,10]]) # vector dimension = 2
+print(array1[1,1])
+print(array1[:,1])
+
+print(array1[1,1:4])
+
+print(array1[-1,:])
+print(array1[:,-1])
+
+
+#### Shape Manipulation #####
+array = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+flatten
+a = array.ravel() # [1,2,3],[4,5,6],[7,8,9] > [1 2 3 4 5 6 7 8 9]
+print(a)
+
+array2 = a.reshape(3,3)
+print(array2)
+
+arrayT = array2.T # transpose
+print(arrayT)
+print(arrayT.shape)
+
+# Difference between reshape() and resize() methods
+array5 = np.array([[1,2],[3,4],[4,5]])
+# reshape()
+print(array5.reshape(2,3)) # array5 didn't changed
+print(array5)
+# resize()
+array5.resize(2,3)
+print(array5)
+
+
+##### Stacking Arrays #####
+array1 = np.array([[1,2],[3,4]])
+array2 = np.array([[-1,-2],[-3,-4]])
+
+# vertical stacking
+# array([1,2],
+#       [3,4])
+# array([-1,-2],
+#       [-3,-4])
+
+array3 = np.vstack((array1,array2))
+print(array3)
+
+# horizontal stacking
+# array([[1,2],[-1,-2],
+#       [3,4],[-3,-4]])
+
+array4 = np.hstack((array1,array2))
+print(array4)
